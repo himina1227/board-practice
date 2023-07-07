64a69986d4603c82fb6c2415d1e3162bba76e20b
nakgyeom <himina1227@gmail.com>
2023-07-07 15:23:15 +0900

    * Spring Data Rest 구현 및 테스트 코드 작성

---
+++ src/test/java/com/example/board/controller/DataRestTest.java
@@ -0,0 +1,34 @@
+package com.example.board.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Transactional
+@AutoConfigureMockMvc // 설정정보를 놓치게 되면 mockMvc가
+@SpringBootTest
+public class DataRestTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void test() throws Exception {
+        mockMvc.perform(get("/api/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")))
+                .andDo(print());
+    }
+}
